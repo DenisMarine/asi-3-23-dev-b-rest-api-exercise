1b543737850cc4d873f68556dd617fdb3ebdb0ad
denism <marine.denis56@gmail.com>
2023-03-25 23:07:29 +0100

    edit pageValidator

--- src/validators.js
+++ src/validators.js
@@ -33,7 +33,7 @@ export const limitValidator = yup.number().integer().min(1).max(100).default(5)
 export const orderValidator = yup.string().lowercase().oneOf(["asc", "desc"])
 export const orderFieldValidator = (fields) => yup.string().oneOf(fields)
 
-export const pageValidator = yup.number().integer().min(1).default(1)
+export const pageValidator = yup.number().integer()
 
 export const filterValidator = (fields) =>
   yup.string().lowercase().oneOf(fields)
